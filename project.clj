d702dab2fe731b7580e3c7b23a9e33ccccac761e
Wilfred Hughes <me@wilfred.me.uk>
2010-11-29 23:07:14 +0000

    basic hello world with jetty

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
-(defproject emailomatic "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject emailomatic "1.0.0-ALPHA"
+  :description "A web scraper that collects interesting events and
+  emails them to the admin."
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+		 [compojure "0.4.0"]
+		 [ring/ring-jetty-adapter "0.2.3"]])
